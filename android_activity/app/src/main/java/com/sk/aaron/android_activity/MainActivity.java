b6861398bc47d72f68cc1bd35b21d7fb08bf6905
aaron <aaron@aarondeMacBook-Pro.local>
2016-10-23 09:31:19 +0800

    Activity传值与回传

--- android_activity/app/src/main/java/com/sk/aaron/android_activity/MainActivity.java
+++ android_activity/app/src/main/java/com/sk/aaron/android_activity/MainActivity.java
@@ -3,28 +3,58 @@ package com.sk.aaron.android_activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static  String key2_msg = "num2_key";
+    public final static  int ReasonCode = 1;
     private Button firstButton;
+    private EditText number1;
+    private EditText number2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         firstButton = (Button)findViewById(R.id.button);
+        number1 = (EditText)findViewById(R.id.editText);
+        number2 = (EditText)findViewById(R.id.editText2);
+
+
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Log.e("--sk", number1.getText().toString());
+                Log.e("--sk", number2.getText().toString());
+
                 Toast.makeText(MainActivity.this, "prepare show first page", Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(MainActivity.this, firPageActivity.class);
-                startActivity(intent);
+
+                intent.putExtra("num1_key", number1.getText().toString());
+                intent.putExtra(key2_msg, number2.getText().toString());
+
+//              带回传结果的意图
+                startActivityForResult(intent, ReasonCode);
+
+//              普通传值
+//                startActivity(intent);
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundleResult = data.getExtras();
+            String sumValue = bundleResult.getString("sum");
+            Log.i("--sk", sumValue);
+        }
     }
 }
--- android_activity/app/src/main/java/com/sk/aaron/android_activity/firPageActivity.java
+++ android_activity/app/src/main/java/com/sk/aaron/android_activity/firPageActivity.java
@@ -1,13 +1,48 @@
 package com.sk.aaron.android_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class firPageActivity extends AppCompatActivity {
 
+    private Button backBtn;
+    private EditText sumValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fir_page);
+
+        Intent intent = getIntent();
+        String key1 = intent.getStringExtra("num1_key");
+        String key2 = intent.getStringExtra(MainActivity.key2_msg);
+
+        TextView desTextView = (TextView) findViewById(R.id.textView5);
+        String msg = key1+"+"+key2+"=";
+        desTextView.setText(msg);
+
+        sumValue = (EditText) findViewById(R.id.editText3);
+
+        backBtn = (Button) findViewById(R.id.button2);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+
+                                           Intent intent = new Intent();
+                                           Bundle bundleBack = new Bundle();
+                                           bundleBack.putString("sum", sumValue.getText().toString());
+                                           intent.putExtras(bundleBack);
+
+                                           setResult(RESULT_OK, intent);
+
+                                           finish();
+                                       }
+                                   }
+        );
     }
 }
